e8fc11d1128b97d5f3f6e3e2a97a5c8504759b9e
Scott Cardinali <ScottCardinali@scotts-mbp.attlocal.net>
2017-10-13 10:17:16 -0400

    updating

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'NaNRRjGkZE4nIpyjT4FHR1Vw9',
+  consumer_secret: 'yGexmDTuvYqjbwWT1dRCdDu4uVCu7EC1268VNJHbs5d6QMyrwK',
+  access_token_key: '917375310659809280-3kl3G5UuDPnk1Vd6AKpUONUVtukzZ9P',
+  access_token_secret: 'LpVrStmtUZ4RYJ9tJHsDzsbwBFsrjLozYDM1XtwJb5tF4',
+}
+
+module.exports = twitterKeys;
+
+var spotifyKeys = {
+  client_ID: 'e6602fe2ef014ab1bd2dfb5fba9d4bf3',
+  client_secret: '9d39f1ded2bd4f2c8ff0baf12600756a',
+}
+
+module.exports = spotifyKeys;
---
+++ liri.js
@@ -0,0 +1,8 @@
+// API keys stored in keys.js file which has been set as a variable
+var twitterKeys = require('/keys.js');
+var spotifyKeys = require('/keys.js');
+
+//
+var twitterpulls = function () {
+  
+}
